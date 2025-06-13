5ca3120d18df68f368e252cc25ebd76d9adedddf
Yumesh Ban <yumeshban365@gmail.com>
2025-06-13 16:21:15 +0545

    Node example

---
+++ Simple Example/app1.js
@@ -0,0 +1,9 @@
+const day = 12;
+
+if (day > 15) {
+  console.log("You have completed 25 % of  the challenge");
+} else {
+  console.log("Few more days before you are there");
+}
+
+console.log("Hey leapfroggers, this is my first node app!");
---
+++ Simple Example/globalexample.js
@@ -0,0 +1,4 @@
+console.log(__dirname);
+setInterval(() => {
+  console.log("Hello from a fellow programmer");
+}, 1000);

2406ff8940d72c6379f5735df17029544b976ee0
Yumesh Ban <yumeshban365@gmail.com>
2025-06-18 17:46:25 +0545

    Project Addition

--- fruit farm/index.js
+++ fruit farm/index.js
@@ -34,19 +34,62 @@ const url = require(`url`);
 
 //SERVER
 
-const data = rs.readFileSync(`./dev-data/data.json`, `utf-8`);
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.butrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = rs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  `utf-8`
+);
+const tempCard = rs.readFileSync(
+  `${__dirname}/templates/tempelate-card.html`,
+  `utf-8`
+);
+const tempProduct = rs.readFileSync(
+  `${__dirname}/templates/tempelate-product.html`,
+  `utf-8`
+);
+
+const data = rs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === `/` || pathName === `/yumesh`) {
-    res.end("Hello my name is yumesh");
+  //Overview page
+
+  if (pathName === `/` || pathName === `/overview`) {
+    res.writeHead(200, { "content-type": `text/html` });
+
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(``);
+    const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHTML);
+    res.end(output);
+
+    //leapfrog page
   } else if (pathName === `/leapfrog`) {
     res.end("This is leapfrog 60 days challenge");
+
+    //API
   } else if (pathName === `/api`) {
     res.writeHead(200, { "content-type": `application/json` });
     res.end(data);
+
+    //NOT FOUND
   } else {
     res.writeHead(404, {
       "Content-type": `text/html`,

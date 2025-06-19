dbeee11cd02145bc7fe8ec5cf3ce3f2f06059e0c
Yumesh Ban <yumeshban365@gmail.com>
2025-06-19 13:37:42 +0545

    Node3

--- fruit farm/index.js
+++ fruit farm/index.js
@@ -4,6 +4,7 @@
 const rs = require("fs");
 const http = require(`http`);
 const url = require(`url`);
+const replaceTemplate = require(`./module/replaceTemplate`);
 
 //FILE
 
@@ -34,22 +35,6 @@ const url = require(`url`);
 
 //SERVER
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.butrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
-
 const tempOverview = rs.readFileSync(
   `${__dirname}/templates/overview.html`,
   `utf-8`
@@ -67,11 +52,12 @@ const data = rs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
   const pathName = req.url;
 
   //Overview page
 
-  if (pathName === `/` || pathName === `/overview`) {
+  if (pathname === `/` || pathname === `/overview`) {
     res.writeHead(200, { "content-type": `text/html` });
 
     const cardsHTML = dataObj
@@ -80,9 +66,12 @@ const server = http.createServer((req, res) => {
     const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHTML);
     res.end(output);
 
-    //leapfrog page
-  } else if (pathName === `/leapfrog`) {
-    res.end("This is leapfrog 60 days challenge");
+    //Product page
+  } else if (pathname === `/product`) {
+    res.writeHead(200, { "content-type": `text/html` });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
   } else if (pathName === `/api`) {

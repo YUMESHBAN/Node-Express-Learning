b950f9721e2580710a1997400e275b80c8bae2e2
Yumesh Ban <yumeshban365@gmail.com>
2025-07-09 14:50:14 +0545

    Handling Exceptions 2

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  // console.log(value);
+  const message = `Duplicate field value:${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data.${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -28,6 +48,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
--- Natours/server.js
+++ Natours/server.js
@@ -4,19 +4,32 @@ dotenv.config({ path: './config.env' });
 
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught exception....Shutting downn....');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  .connect(DB)
-  .then(() => console.log('DB connection successful'))
-  .catch((err) => console.error('DB connection error:', err));
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
+// .catch((err) => console.error('DB connection error:', err));
 
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running in Port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection....Shutting downn....');
+  server.close(() => {
+    process.exit(1);
+  });
 });

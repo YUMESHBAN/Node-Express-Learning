bcdefc95c1486fbe582053bcd536c1d0ba5deac5
Yumesh Ban <yumeshban365@gmail.com>
2025-07-13 13:09:24 +0545

    Password Reset Functionality

--- Natours/models/userModel.js
+++ Natours/models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 
@@ -40,6 +41,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  PasswordResetToken: String,
+  passwordResetExpires: Date,
 });
 userSchema.pre('save', async function (next) {
   //Only run when password is modified
@@ -69,6 +72,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     return JWTTimestamp < changeTimestamp;
   }
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.PasswordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.PasswordResetToken);
+
+  this.passwordResetExpires = Date.now() + 1 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
